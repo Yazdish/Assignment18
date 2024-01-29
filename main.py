305eaff8b0ea8077c436b0ce8205b81d77287717
Yazdan Shouryabi <yazdishouryabi@gmail.com>
2024-01-29 15:36:46 +0330

    Raw

--- main.py
+++ main.py
@@ -1,32 +1,44 @@
 import sys
 from functools import partial
-from PySide6.QtWidgets import QApplication
+from PySide6.QtWidgets import QApplication, QMessageBox
 from PySide6.QtUiTools import QUiLoader
 
+def check():
+    if buttons[0][0].text() == "X" and buttons[0][1].text() == "X" and buttons[0][2].text() == "X":
+        msg_box = QMessageBox(text="Player 1 Wins")
+        msg_box.exec()
 
 def play(row, col):
-    buttons[row][col].setText("X")
+    global player
+    if player == 1:
+        buttons[row][col].setText("X")
+        buttons[row][col].setStyleSheet("color: red; background-color: white")
+        player = 2
+
+    elif player == 2:
+        buttons[row][col].setText("O")
+        buttons[row][col].setStyleSheet("color: blue; background-color: white")
+        player = 1
+    
+    check()
 
 
 
 app = QApplication(sys.argv)
 
+player = 1
+
 loader = QUiLoader()
 main_window = loader.load("main_window.ui")
 main_window.show()
-main_window.btn_1.clicked.connect(partial(play, 0, 0))
-main_window.btn_2.clicked.connect(partial(play, 0, 1))
-main_window.btn_3.clicked.connect(partial(play, 0, 2))
-main_window.btn_4.clicked.connect(partial(play, 1, 0))
-main_window.btn_5.clicked.connect(partial(play, 1, 1))
-main_window.btn_6.clicked.connect(partial(play, 1, 2))
-main_window.btn_7.clicked.connect(partial(play, 2, 0))
-main_window.btn_8.clicked.connect(partial(play, 2, 1))
-main_window.btn_9.clicked.connect(partial(play, 2, 2))
 
 buttons = [[main_window.btn_1, main_window.btn_2, main_window.btn_3],
            [main_window.btn_4, main_window.btn_5, main_window.btn_6],
            [main_window.btn_7, main_window.btn_8, main_window.btn_9]]
+
+for i in range (3):
+    for j in range (3):
+        buttons[i][j].clicked.connect(partial(play, i, j))
 
 
 
